64655270ac3ef051a82fc89a5b4f2696f9f51ca1
Józsa Viktor <113334706+jozsaviktor@users.noreply.github.com>
2023-10-09 13:06:36 +0200

    Add files via upload

---
+++ szamonkeres01.py
@@ -0,0 +1,15 @@
+print('Józsa Viktor')
+jegy = int(input("Kérek egy jegyet 1-5-ig: "))
+
+if jegy== 1:
+    print('a jegyed', jegy, 'ami elégtelen')
+elif jegy== 2:
+    print('a jegyed', jegy, 'ami elégséges')
+elif jegy== 3:
+    print('a jegyed', jegy, 'ami közepes')
+elif jegy== 4:
+    print('a jegyed', jegy, 'ami jó')
+elif jegy== 5:
+    print('a jegyed', jegy, 'ami jeles')
+elif jegy==6:
+    print('a jegyed', jegy, 'ami nem megfelelő')
